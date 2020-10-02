05f945206f7925fc41f566f4060347b4181d7ec6
johnlfreire <john-jtw@hotmail.com>
2020-10-02 18:35:22 -0300

    Commit inicial do projeto

---
+++ sfb-Admin-Client/src/main/java/com/br/sfb/SfbAdminClientApplication.java
@@ -0,0 +1,14 @@
+package com.br.sfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class SfbAdminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfbAdminClientApplication.class, args);
+	}
+
+}
